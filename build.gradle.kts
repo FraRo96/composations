cedb6c7913088a04c195e7bdae3e1832b0642ef9
Francesco Rossini <francesco.rossini@linksfoundation.com>
2025-01-20 00:01:17 +0100

    composable-realtime-animations v1.0.0 🎉 (removed app plugin)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,7 @@ buildscript {
     }
 }
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.compose.compiler) apply false
